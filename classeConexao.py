3a5d2e381d2e9cd87a3a937f63ff24e61c2b8152
Leandro Afonso da Silva de Almeida <leandroslv125@gmail.com>
2022-04-14 20:29:42 -0300

    Correção

--- classeConexao.py
+++ classeConexao.py
@@ -1,5 +1,4 @@
-
-import  mysql.connector
+import mysql.connector
 
 class Conexao():
     def __init__(self,host,banco,usuario,senha):
@@ -7,4 +6,120 @@ class Conexao():
         self.banco = banco
         self.usuario = usuario
         self.senha = senha
-        
+
+    def select(self, pTabela, pCampos, pClausula = ['']):
+        conn = mysql.connector.connect(host=self.host,user=self.usuario,database=self.banco,passwd=self.senha)        
+        if conn.is_connected():
+            cursor = conn.cursor()
+            sql = 'SELECT '
+            sqlC = ''
+            for i in range(0,len(pCampos)):
+                sqlC += pCampos[i] + ', ' 
+            sqlC = sqlC[:-2] + 'VALUES ('
+            sqlV = ' '
+            sql += sqlC + ' FROM ' + pTabela
+            sqlW = ''            
+            if pClausula[0] != '':
+                for i in range(0,len(pClausula)):
+                    if i == 0:
+                        sqlW = ' WHERE ' + pClausula[i]
+                    else:
+                        sqlW += ' AND ' + pClausula[i]
+            # if len(pClausula) != 0:
+            #     sql += ' ' + pClausula
+            sql += sqlW + ';'
+            print(sql)
+            return
+            cursor.execute(sql)
+            total = 0
+            for linha in cursor:
+                total += 1
+            if total < 1:
+                print(pTabela +' não possui registros!')
+            else:
+                print('Registros da tabela ' + pTabela)
+                cursor.execute(sql)
+                for linha in cursor:
+                    print(linha)                    
+                print('Total de registros: ' + str(cursor.rowcount))
+        else:
+            print('Não está conectado ao banco de dados')
+    
+    def insert(self,pTabela,pCampos,pValores):
+        conn = mysql.connector.connect(host=self.host,user=self.usuario,database=self.banco,passwd=self.senha)  
+        if conn.is_connected():
+            cursor = conn.cursor()
+            sql = 'INSERT INTO '+ pTabela + ' (' 
+            sqlC=''
+            for i in range(0,len(pCampos)):
+                sqlC += pCampos[i]+', '
+            sqlC = sqlC[:-2] + ') '
+            sql += sqlC + ' VALUES ('
+            sqlV = ' '
+            for i in range (0, len(pValores)):
+                if type(pValores[i]) is str:
+                    sqlV +='"'+ pValores[i] + '", '
+                else:
+                    sqlV += str(pValores[i])+ ', '
+            sqlV= sqlV[:-2] + '); '
+            sql += sqlV
+            cursor.execute(sql)
+            try:
+                conn.commit()
+                print('Registro inserido com sucesso')
+            except:
+                print('Erro ao inserir o registro')
+        else:
+            print('Não está conectado ao banco de dados')
+    
+    def delete(self,pTabela,pClausula=['']):
+        conn = mysql.connector.connect(host=self.host,user=self.usuario,database=self.banco,passwd=self.senha)
+        if self.conn.is_connected():
+            cursor = self.conn.cursor()
+            sql = 'DELETE FROM' + pTabela
+            sqlW = ''
+            if pClausula[0] != '':
+               sql +='WHERE'
+               for i in range(0,len(pClausula)):
+                   if i == 0:
+                       sqlW = pClausula[i]
+                   else:
+                        sqlW += ' AND' + pClausula[i]
+            sqlW = sqlW
+            sql += sqlW + ';'
+            print(sql)
+            return
+            #sql = 'DELETE FROM conta_corrente WHERE id_conta_corrente = 4'
+            cursor.execute(sql)
+            try:
+                self.conn.commit()
+                print('Registro excluído com sucesso')
+            except:
+                print('Erro ao excluir o registro')
+        else:
+            print('Não está conectado ao banco de dados')
+    
+    def update(self):
+        if self.conn.is_connected():
+            cursor = self.conn.cursor()
+            sql = 'UPDATE conta_corrente SET numero_conta_corrente = "54321" WHERE id_conta_corrente = 5'
+            cursor.execute(sql)
+            try:
+                self.conn.commit()
+                print('Registro alterado com sucesso')
+            except:
+                print('Erro ao alterar o registro')
+        else:
+            print('Não está conectado ao banco de dados')
+
+conexao = Conexao('localhost','banco','root','1234')
+
+#campos = ['numero_conta_corrente','saldo_conta_corrente']
+#clausula = ['1=1','id_conta_corrente >= 2','numero_conta_corrente != ""']
+campos = ['numero_conta_poupanca','saldo_conta_poupanca']
+valores =['98765',10.50]
+#clausula = ['id_conta_corrente = 3','numero_conta_corrente is not null','1=1']
+#conexao.select('conta_corrente',campos,clausula)
+conexao.insert('conta_poupanca',campos,valores)
+#conexao.delete('conta_corrente',clausula)
+
